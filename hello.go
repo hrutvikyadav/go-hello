ca02ceee687d94f30a1d92782e4e2193fb6ac21d
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-09-11 22:49:06 +0530

    feat: checking for and logging errors

--- hello.go
+++ hello.go
@@ -3,12 +3,20 @@ package main
 import (
     "fmt"
     "hrutvik.gg/go_greet"
+    "log"
 )
 
 import "rsc.io/quote/v4"
 
 
 func main() {
-    msg := go_greet.Greet("Hrutvik")
+    log.SetPrefix("[6GREETER9]: ")
+    log.SetFlags(0)
+
+    msg, err := go_greet.Greet("")
+    if err != nil {
+        log.Fatal(err)
+    }
+
     fmt.Println(msg, "\n", quote.Go())
 }
